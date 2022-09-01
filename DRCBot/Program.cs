1e043d60fabbb828a14dfc2c3eefcb42f8d8d706
Joe Sorensen <hello@murl.digital>
2022-09-01 14:25:00 -0600

    initial commit

---
+++ DRCBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using DRCBot.Responders;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Remora.Discord.API.Abstractions.Gateway.Commands;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Gateway.Commands;
+using Remora.Discord.API.Objects;
+using Remora.Discord.Gateway;
+using Remora.Discord.Gateway.Extensions;
+using Remora.Discord.Hosting.Extensions;
+
+var host = Host
+    .CreateDefaultBuilder()
+    .AddDiscordService(services => services.GetService<IConfiguration>().GetRequiredSection("Discord").GetValue<string>("Token"))
+    .ConfigureServices(services =>
+    {
+        services.Configure<DiscordGatewayClientOptions>(options =>
+        {
+            options.Intents |= GatewayIntents.GuildMessageReactions;
+            options.Presence = new UpdatePresence(ClientStatus.Idle, false, DateTimeOffset.Now, new IActivity[]
+            {
+                new Activity("saus!!", ActivityType.Game)
+            });
+        });
+        services.AddResponder<MessageReactionResponder>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ DRCBot/Responders/MessageReactionResponder.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+
+namespace DRCBot.Responders;
+
+public class MessageReactionResponder : IResponder<IMessageReactionAdd>, IResponder<IMessageReactionRemove>
+{
+    private readonly ILogger<MessageReactionResponder> _logger;
+
+    public MessageReactionResponder(ILogger<MessageReactionResponder> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<Result> RespondAsync(IMessageReactionAdd gatewayEvent, CancellationToken ct = new())
+    {
+        if (!gatewayEvent.GuildID.HasValue)
+            return Result.FromError(new ArgumentInvalidError("gatewayEvent.GuildID", $"gatewayEvent has no guild id. event: {gatewayEvent}"));
+
+        return Result.FromSuccess();
+    }
+
+    public async Task<Result> RespondAsync(IMessageReactionRemove gatewayEvent, CancellationToken ct = new())
+    {
+        if (!gatewayEvent.GuildID.HasValue)
+            return Result.FromError(new ArgumentInvalidError("gatewayEvent.GuildID", $"gatewayEvent has no guild id. event: {gatewayEvent}"));
+
+        return Result.FromSuccess();
+    }
+}
