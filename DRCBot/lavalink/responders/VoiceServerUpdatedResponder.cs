351c2b01c79cc1a4fa38f80225eb174c45ae3df8
Joe Sorensen <hello@murl.digital>
2022-09-11 16:27:21 -0600

    added responders to wire up events for lavalink4net

---
+++ DRCBot/lavalink/responders/VoiceServerUpdatedResponder.cs
@@ -0,0 +1,30 @@
+﻿using Lavalink4NET;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+
+namespace DRCBot.lavalink.responders;
+
+public class VoiceServerUpdatedResponder : IResponder<IVoiceServerUpdate>
+{
+    private readonly IDiscordClientWrapper _discordClientWrapper;
+
+    public VoiceServerUpdatedResponder(IDiscordClientWrapper discordClientWrapper)
+    {
+        _discordClientWrapper = discordClientWrapper;
+    }
+    
+    public async Task<Result> RespondAsync(IVoiceServerUpdate gatewayEvent, CancellationToken ct = new CancellationToken())
+    {
+        if (gatewayEvent.Endpoint is null)
+            return Result.FromError(new ArgumentNullError("gatewayEvent.Token"));
+        
+        if (_discordClientWrapper is not RemoraClientWrapper remoraClientWrapper)
+            return Result.FromError(new ArgumentInvalidError("_discordClientWrapper", "MUST be RemoraClientWrapper"));
+        
+        await remoraClientWrapper.InvokeVoiceServerUpdated(gatewayEvent.GuildID.Value, gatewayEvent.Token,
+            gatewayEvent.Endpoint);
+        
+        return Result.FromSuccess();
+    }
+}
---
+++ DRCBot/lavalink/responders/VoiceStateUpdatedResponder.cs
@@ -0,0 +1,26 @@
+﻿using Lavalink4NET;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.API.Gateway.Events;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+
+namespace DRCBot.lavalink.responders;
+
+public class VoiceStateUpdatedResponder : IResponder<IVoiceStateUpdate>
+{
+    private readonly IDiscordClientWrapper _discordClientWrapper;
+
+    public VoiceStateUpdatedResponder(IDiscordClientWrapper discordClientWrapper)
+    {
+        _discordClientWrapper = discordClientWrapper;
+    }
+    public async Task<Result> RespondAsync(IVoiceStateUpdate gatewayEvent, CancellationToken ct = new CancellationToken())
+    {
+        if (_discordClientWrapper is not RemoraClientWrapper remoraClientWrapper)
+            return Result.FromError(new ArgumentInvalidError("_discordClientWrapper", "MUST be RemoraClientWrapper"));
+
+        await remoraClientWrapper.InvokeVoiceStateUpdated(gatewayEvent.UserID.Value, gatewayEvent.GuildID.Value.Value, gatewayEvent.ChannelID?.Value ?? null, gatewayEvent.SessionID);
+
+        return Result.FromSuccess();
+    }
+}
