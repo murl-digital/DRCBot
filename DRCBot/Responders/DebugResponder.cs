7199b21412869ae688566bb624f5d9bec0571774
Joe Sorensen <hello@murl.digital>
2022-09-11 17:27:36 -0600

    added debug responder

---
+++ DRCBot/Responders/DebugResponder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+
+namespace DRCBot.lavalink.responders;
+
+public class DebugResponder : IResponder<IGatewayEvent>
+{
+    private readonly ILogger<DebugResponder> _logger;
+
+    public DebugResponder(ILogger<DebugResponder> logger)
+    {
+        _logger = logger;
+    }
+    public async Task<Result> RespondAsync(IGatewayEvent gatewayEvent, CancellationToken ct = new ()) {
+        _logger.LogDebug("got event: {}", gatewayEvent);
+        
+        return Result.FromSuccess();
+    }
+}
