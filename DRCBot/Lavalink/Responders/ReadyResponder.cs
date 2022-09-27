459186f3eef7e5f564a54a73b226f8eb832fccb1
Joe Sorensen <hello@murl.digital>
2022-09-26 20:10:43 -0600

    lovely error catching boilerplate

--- DRCBot/Lavalink/Responders/ReadyResponder.cs
+++ DRCBot/Lavalink/Responders/ReadyResponder.cs
@@ -1,5 +1,7 @@
-﻿using Lavalink4NET;
+﻿using System.Net.WebSockets;
+using Lavalink4NET;
 using Lavalink4NET.Tracking;
+using Microsoft.Extensions.Logging;
 using Remora.Discord.API.Abstractions.Gateway.Events;
 using Remora.Discord.Gateway.Responders;
 using Remora.Results;
@@ -8,18 +10,36 @@ namespace DRCBot.Responders;
 
 public class ReadyResponder : IResponder<IReady>
 {
+    private readonly ILogger<ReadyResponder> _logger;
     private readonly IAudioService _audioService;
     private readonly InactivityTrackingService _inactivityTrackingService;
 
-    public ReadyResponder(IAudioService audioService, InactivityTrackingService inactivityTrackingService)
+    public ReadyResponder(ILogger<ReadyResponder> logger, IAudioService audioService, InactivityTrackingService inactivityTrackingService)
     {
+        _logger = logger;
         _audioService = audioService;
         _inactivityTrackingService = inactivityTrackingService;
     }
     public async Task<Result> RespondAsync(IReady gatewayEvent, CancellationToken ct = new CancellationToken())
     {
-        await _audioService.InitializeAsync();
-        _inactivityTrackingService.BeginTracking();
+        try
+        {
+            await _audioService.InitializeAsync();
+        }
+        catch (Exception e)
+        {
+            _logger.LogWarning(e, "There was a problem connecting to Lavalink, music playing won't be available");
+        }
+        
+        try
+        {
+            if (!_inactivityTrackingService.IsTracking)
+                _inactivityTrackingService.BeginTracking();
+        }
+        catch (Exception e)
+        {
+            _logger.LogWarning(e, "There was a problem beginning inactivity tracking");
+        }
 
         return Result.FromSuccess();
     }
