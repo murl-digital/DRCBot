76da2b679db4c92af95d6e3cd96dc78112f3f030
Joe Sorensen <hello@murl.digital>
2022-09-26 20:48:04 -0600

    post execution update

--- DRCBot/Commands/PostExecutionEventLogger.cs
+++ DRCBot/Commands/PostExecutionEventLogger.cs
@@ -1,6 +1,8 @@
 ﻿using System.Text.Json;
 using Microsoft.Extensions.Logging;
+using Remora.Discord.API.Abstractions.Objects;
 using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.API.Objects;
 using Remora.Discord.Commands.Contexts;
 using Remora.Discord.Commands.Services;
 using Remora.Rest.Core;
@@ -18,18 +20,31 @@ public class PostExecutionEventLogger : IPostExecutionEvent
         _logger = logger;
         _interactionApi = interactionApi;
     }
+
     public async Task<Result> AfterExecutionAsync(ICommandContext context, IResult commandResult,
         CancellationToken ct = new CancellationToken())
     {
         if (commandResult.IsSuccess) return Result.FromSuccess();
         var errorCode = Snowflake.CreateTimestampSnowflake().Value;
-        _logger.LogWarning("application command failed. lookup code {}. error: {}", errorCode, JsonSerializer.Serialize(commandResult));
+        _logger.LogWarning("application command failed. lookup code {@0}. error: {@1}", errorCode,
+            JsonSerializer.Serialize(commandResult.Error));
+
         if (context is not InteractionContext interactionContext) return Result.FromSuccess();
+        if (!(await _interactionApi.GetOriginalInteractionResponseAsync(interactionContext.ApplicationID,
+                interactionContext.Token, ct)).IsSuccess)
+            return await _interactionApi.CreateInteractionResponseAsync(interactionContext.ApplicationID,
+                interactionContext.Token,
+                new InteractionResponse(InteractionCallbackType.ChannelMessageWithSource,
+                    new(new InteractionMessageCallbackData(
+                        Content:
+                        $"There was a problem running this command. Make sure that your parameters are correct (e.g you're using a valid emoji). If they are, report this code to DRACONIUM: {errorCode.ToString()}"))),
+                ct: ct);
+
         var responseResult = await _interactionApi.CreateFollowupMessageAsync(interactionContext.ApplicationID,
             interactionContext.Token,
-            $"There was a problem running this command. Make sure that your parameters are correct (e.g you're using a valid emoji). If they are, report this code to DRACONIUM: {errorCode.ToString()}", ct: ct);
-                
-        return responseResult.IsSuccess ? Result.FromSuccess() : Result.FromError(responseResult.Error);
+            $"There was a problem running this command. Make sure that your parameters are correct (e.g you're using a valid emoji). If they are, report this code to DRACONIUM: {errorCode.ToString()}",
+            ct: ct);
 
+        return responseResult.IsSuccess ? Result.FromSuccess() : Result.FromError(responseResult.Error);
     }
 }
